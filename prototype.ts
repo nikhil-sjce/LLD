e7bdef255ded3e0be11ae187b69422f0d128ff1a
Nikhil Vyas <65950622+nikhil-sjce@users.noreply.github.com>
2025-12-13 14:27:12 +0530

    Update prototype.ts

--- prototype.ts
+++ prototype.ts
@@ -1,1 +1,20 @@
+interface Shape{
+    clone(): Shape;
+    getDetails(): void;
+}
 
+class ICircle implements Shape{
+    constructor(public radius: string){};
+    clone() : ICircle{
+        return new ICircle(this.radius);
+    }
+    getDetails(): void{
+        console.log(`Circle with radius: ${this.radius}`);
+    }
+}
+
+const circleObject1 = new ICircle("5");
+const circleObject2 = circleObject1.clone();
+
+circleObject1.getDetails();
+circleObject2.getDetails();
